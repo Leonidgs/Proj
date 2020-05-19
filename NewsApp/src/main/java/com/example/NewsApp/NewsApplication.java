eb80fb26bcfd6f9711183f2c5f60ddcec098ff80
egorrussia <egorkonstantinov@gmail.com>
2020-05-19 20:27:53 +0300

    create project

--- NewsApp/src/main/java/com/example/NewsApp/NewsApplication.java
+++ NewsApp/src/main/java/com/example/NewsApp/NewsApplication.java
@@ -3,16 +3,11 @@ package com.example.NewsApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.NewsApp.model.Articles;
-
 @SpringBootApplication
 public class NewsApplication {
 
 	public static void main(String[] args) {
 		
-		var articles = new Articles();
-		
-	
 		SpringApplication.run(NewsApplication.class, args);
 	}
 
