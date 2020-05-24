4a3baa0f431d0e991160b73f11a5254a9bf4f30e
iancioglo <stuyan24@gmail.com>
2020-05-24 18:41:26 +0300

    main page

--- NewsApp/src/main/java/com/example/NewsApp/controller/WebController.java
+++ NewsApp/src/main/java/com/example/NewsApp/controller/WebController.java
@@ -3,8 +3,10 @@ package com.example.NewsApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.NewsApp.model.articles.Articles;
 import com.example.NewsApp.service.IRequestHandler;
 
 @Controller
@@ -18,7 +20,9 @@ public class WebController {
 	
 	
 	@GetMapping("/news")
-	public String getMainPage() {
+	public String getMainPage(Model model) {
+		Articles articles = handler.requestForObject(url, Articles.class);
+		model.addAttribute("articles", articles);
 		return "main";
 	}
 
